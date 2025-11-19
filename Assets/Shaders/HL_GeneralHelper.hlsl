f4053b4bd1305edcd2e05419c74b3fa757687956
Alex <wangtianyoualex@gmail.com>
2025-11-18 16:00:40 -0800

    update world quad mask function

--- Assets/Shaders/HL_GeneralHelper.hlsl
+++ Assets/Shaders/HL_GeneralHelper.hlsl
@@ -1,9 +1,30 @@
 #ifndef HL_GENERAL_HELPER
 #define HL_GENERAL_HELPER
 
-float DistanceTravelToReachPlaneDefinedByNormalFromWorldSpace(float3 planeNormal, float3 planeOrigin, float3 worldPos, float3 viewVector)
+float DistanceTravelToReachPlaneDefinedByRayFromWorldSpace(float3 planeNormal, float3 planeOrigin, float3 worldPos, float3 viewVector)
 {
-    return dot(planeNormal, (worldPos - planeOrigin)) / dot(planeNormal, viewVector);
+    // the Quad can be defined as: dot(planeNormalWS, P - planeOriginWS) = 0
+    // also P = posWS - viewDirWS * t, solve t
+    return dot(planeNormal, (worldPos - planeOrigin)) * rcp(dot(planeNormal, viewVector));
 }
 
+float WorldSpaceQuadMask(float3 posWS, float3 viewDirWS, float3 camPosWS, float3 quadOriginWS, float3 quadBoundaryXEndWS, float3 quadBoundaryYEndWS)
+{
+    float3 originToXWS = quadBoundaryXEndWS - quadOriginWS;
+    float3 originToYWS = quadBoundaryYEndWS - quadOriginWS;
+    float3 quadNormal = normalize(cross(originToXWS, originToYWS));
+
+    float t = DistanceTravelToReachPlaneDefinedByRayFromWorldSpace(quadNormal, quadOriginWS, posWS, viewDirWS);
+    float3 plane = posWS - viewDirWS * t;
+    // clip the plane to bounding box
+    float2 quadSpanLocalSpace = float2(dot(normalize(originToXWS), plane - quadOriginWS), dot(normalize(originToYWS), plane - quadOriginWS));
+    
+    bool inSide =
+            quadSpanLocalSpace.x >= 0
+        && quadSpanLocalSpace.x <= length(originToXWS)
+        && quadSpanLocalSpace.y >= 0
+        && quadSpanLocalSpace.y <= length(originToYWS);
+    
+    return !inSide ? 1 : distance(camPosWS, posWS) > distance(camPosWS, plane) ? 0 : 1;
+}
 #endif
